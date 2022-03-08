10e061f16f4c3c6a1cc865bdff46b4bee61637ed
nicolawebb <nicolaptv@gmail.com>
2022-03-08 17:00:10 +0000

    Update with css

--- helloWorldForm.py
+++ helloWorldForm.py
@@ -10,12 +10,18 @@ form = cgi.FieldStorage()
 name = form.getvalue('name')        # name of the field is same as in client form
 module  = form.getvalue('module')   # name of the field is same as in client form
 
-print ("Content-type:text/html\n")
-print ("<HTML>")
-print ("<HEAD>")
-print ("<TITLE>Hello - Form processing</TITLE>")
-print ("</HEAD>")
-print ("<BODY>")
-print ("<H1>Hello ", name, ". You're enrolled to study ", module, ". Enjoy!</H1>")
-print ("</BODY>")
-print ("</HTML>")
+
+html = """
+<html>
+<head>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<h1>Hello {name}. You're enrolled to study {module}. Enjoy!</h1>
+</body>
+</html>
+""".format(
+        name=name,
+        module=module
+        )
+print(html)
