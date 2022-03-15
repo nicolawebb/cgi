a665e57edfeb25b485f046a14784455ad8497312
n22-webb <nicola5.webb@live.uwe.ac.uk>
2022-03-15 16:39:26 +0000

    Update regex to include python examples

--- regex/reg.py
+++ regex/reg.py
@@ -1,22 +1,49 @@
 import re
 
-# pattern = '^p....n$'
-# test_string = 'python'
-# result = re.search(pattern, test_string)
+###========SEARCH==========###
+test_string = 'python'
 
-# if result:
-#   print("Search successful.")
-# else:
-#   print("Search unsuccessful.")	
+##with python
+starts_with = test_string.startswith('p')
+ends_with = test_string.endswith('n')
+if starts_with and ends_with:
+    print("Search successful.")
+else:
+  print("Search unsuccessful.")	
+
+##with regex
+pattern = '^p....n$'
+result = re.search(pattern, test_string)
+if result:
+  print("Search successful.")
+else:
+  print("Search unsuccessful.")	
 
 
-# test_string = "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."
+###========FIND ALL==========###
+test_string = "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."
 
-# x = re.findall("Sith", test_string)
-# print(x)
+##with python
+idx = 0
+while idx < len(test_string):
+        idx = test_string.find('Sith', idx)
+        if idx == -1:
+            break
+        print('Sith at:', idx)
+        idx += 1
+
+##with regex
+x = re.findall("Sith", test_string)
+print(x)
 
 
+###========SPLIT==========###
 test_string = "The rain in Spain"
-split_string = re.split("\s", test_string)
+
+##with python
+split_string = test_string.split(" ")
 print(split_string)
 
+##with regex
+split_string = re.split("\s", test_string)
+print(split_string)
