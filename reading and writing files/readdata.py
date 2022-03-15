b02cdf081c32ed88a1e82ad1017e0b7a2654a834
n22-webb <nicola5.webb@live.uwe.ac.uk>
2022-03-15 16:06:32 +0000

    Uncomment file

--- reading and writing files/readdata.py
+++ reading and writing files/readdata.py
@@ -2,24 +2,24 @@ from fileinput import filename
 import os
 
 #path to text file
-# file_name = 'reading and writing files/data.txt'
+file_name = 'reading and writing files/data.txt'
 
 # #read file
-# read_file = open(file_name, 'r')
-# for line in read_file:
-#     print(line)
-# read_file.close()
+read_file = open(file_name, 'r')
+for line in read_file:
+    print(line)
+read_file.close()
 
 # #append line to file
-# append_file = open(file_name, 'a')
-# append_file.write("MNO345, Research Methods, E17, 25")
-# append_file.close()
+append_file = open(file_name, 'a')
+append_file.write("MNO345, Research Methods, E17, 25")
+append_file.close()
 
 # #write line to file - overrides existing file content
-# file_name2 = 'reading and writing files/deletecontents.txt'
-# write_file = open(file_name2, 'w')
-# write_file.write(":(")
-# write_file.close()
+file_name2 = 'reading and writing files/deletecontents.txt'
+write_file = open(file_name2, 'w')
+write_file.write(":(")
+write_file.close()
 
 #delete a file
 file_name3 = 'reading and writing files/delete.txt'
